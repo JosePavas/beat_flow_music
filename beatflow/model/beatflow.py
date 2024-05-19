d21fa250ae9fd0db0e733a5b2d7cea2a05558ac4
JosePavas <jpavas887@soyudemedellin.edu.co>
2024-05-18 23:13:11 -0500

    actualizacion, se agrega las canciones

--- beatflow/model/beatflow.py
+++ beatflow/model/beatflow.py
@@ -1,5 +1,37 @@
+import vlc
+import time
+import os
 import random
 import sys
+
+
+class Canciones:
+    
+    def __init__(self):
+        self.directory = directory
+    
+    def get_mp3_files(self):
+    # Lista para almacenar los archivos mp3
+        mp3_files = []
+
+    # Iterar sobre los archivos en el directorio
+        for filename in os.listdir(directory):
+        # Comprobar si el archivo tiene extensión .mp3
+            if filename.endswith('.mp3'):
+            # Agregar el archivo a la lista
+                mp3_files.append(os.path.join(directory, filename))
+
+        return mp3_files
+
+# Ruta del directorio que contiene las canciones
+directory = 'BEATFLOW.musica'
+
+# Obtener la lista de archivos mp3
+mp3_files = Canciones.get_mp3_files(directory)
+
+# Imprimir la lista de archivos mp3
+for mp3 in mp3_files:
+    print(mp3)
 
 class Cancion:
     def __init__(self, nombre, artista):
@@ -18,14 +50,6 @@ class ListaReproduccion:
         self.canciones.append(cancion)
         print(f"Canción '{cancion}' agregada a la lista '{self.nombre}'.")
     
-    # def agregar_cancion(self, cancion, nombre_lista):
-    #     if nombre_lista in self.listas_reproduccion:
-    #         lista = self.listas_reproduccion[nombre_lista]
-    #         lista.agregar_cancion(cancion)
-    #         print(f"Canción '{cancion}' agregada a la lista '{self.nombre}'.")
-    #     else:
-    #         print("No existe una lista de reproducción con ese nombre.")
-        
     def eliminar_cancion(self, indice):
         if 0 <= indice < len(self.canciones):
             cancion_eliminada = self.canciones.pop(indice)
@@ -56,14 +80,6 @@ class BeatFlow:
         print("\nListas de reproducción disponibles:")
         for nombre_lista in self.listas_reproduccion:
             print(nombre_lista)
-
-    # def agregar_cancion(self, cancion, nombre_lista):
-    #     if nombre_lista in self.listas_reproduccion:
-    #         lista = self.listas_reproduccion[nombre_lista]
-    #         lista.agregar_cancion(cancion)
-    #         print(f"Canción '{cancion}' agregada a la lista '{self.nombre}'.")
-    #     else:
-    #         print("No existe una lista de reproducción con ese nombre.")
     
     def agregar_cancion(self, nombre_lista, cancion):
         if nombre_lista in self.listas_reproduccion:
@@ -127,4 +143,67 @@ class BeatFlow:
         print("Reproduciendo en orden aleatorio:")
         for cancion in self.cola_reproduccion:
             print(cancion)
+class MusicPlayer:
+    def __init__(self):
+        self.instance = vlc.Instance()
+        self.player = self.instance.media_player_new()
+        self.playlist = []
+        self.current_index = 0
+    
+    def add_song(self, song):
+        self.playlist.append(song)
+    
+    def play_song(self, index):
+        if index < len(self.playlist):
+            media = self.instance.media_new(self.playlist[index])
+            self.player.set_media(media)
+            self.player.play()
+            self.current_index = index
+            time.sleep(1)  # Espera un momento para asegurar que la reproducción comience
 
+    def play_next(self):
+        if self.current_index + 1 < len(self.playlist):
+            self.play_song(self.current_index + 1)
+
+    def play_previous(self):
+        if self.current_index - 1 >= 0:
+            self.play_song(self.current_index - 1)
+
+    def stop(self):
+        self.player.stop()
+
+    def set_volume(self, volume):
+        if 0 <= volume <= 100:
+            self.player.audio_set_volume(volume)
+    
+    def get_volume(self):
+        return self.player.audio_get_volume()
+    
+    def add_to_queue(self, song):
+        self.add_song(song)
+
+    def play_queue(self):
+        while self.current_index < len(self.playlist):
+            self.play_song(self.current_index)
+            while self.player.is_playing():
+                time.sleep(1)
+            self.current_index += 1
+
+# Ejemplo de uso del reproductor de música
+player = MusicPlayer()
+player.add_song('song1.mp3')
+player.add_song('song2.mp3')
+player.add_song('song3.mp3')
+
+# Reproducir la lista de canciones
+player.play_queue()
+
+# Subir y bajar volumen
+current_volume = player.get_volume()
+print(f'Volume before: {current_volume}')
+player.set_volume(current_volume + 10)
+print(f'Volume after: {player.get_volume()}')
+
+# Agregar una canción a la cola y reproducir
+player.add_to_queue('song4.mp3')
+player.play_queue()
